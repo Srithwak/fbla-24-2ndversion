ddca0a4b8bbcd12a329f08f8af1bb47b1a9b40cc
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2024-01-09 16:41:25 -0500

    header fiNAL (prolly not lol)

--- src/assets/helper/helper.js
+++ src/assets/helper/helper.js
@@ -4,24 +4,24 @@ const path = require('path')
 
 
 // HEADER STUFF
-function toggleIcon() {
-   const icon = document.querySelector('i');
-   const state = icon.getAttribute('data-state');
-   if (state === 'closed') {
-      icon.classList.remove('fa-bars');
-      icon.classList.add('fa-times');
-      icon.setAttribute('data-state', 'open');
-   } else {
-      icon.classList.remove('fa-times');
-      icon.classList.add('fa-bars');
-      icon.setAttribute('data-state', 'closed');
-   }
-}
+// function toggleIcon() {
+//    const icon = document.querySelector('i');
+//    const state = icon.getAttribute('data-state');
+//    if (state === 'closed') {
+//       icon.classList.remove('fa-bars');
+//       icon.classList.add('fa-times');
+//       icon.setAttribute('data-state', 'open');
+//    } else {
+//       icon.classList.remove('fa-times');
+//       icon.classList.add('fa-bars');
+//       icon.setAttribute('data-state', 'closed');
+//    }
+// }
 
-function toggleNav() {
-   document.body.classList.toggle("nav-open");
-   toggleIcon();
-}
+// function toggleNav() {
+//    document.body.classList.toggle("nav-open");
+//    toggleIcon();
+// }
 
 
 
